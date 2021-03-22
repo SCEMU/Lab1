cf3b693e1528fb64f7433cb190a9366268dc9f23
SCEMU <71563287+SCEMU@users.noreply.github.com>
2021-03-22 10:19:58 +0300

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace WindowsFormsApp1
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.button1 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      this.button3 = new System.Windows.Forms.Button();
+      this.button4 = new System.Windows.Forms.Button();
+      this.button5 = new System.Windows.Forms.Button();
+      this.button6 = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.button7 = new System.Windows.Forms.Button();
+      this.button8 = new System.Windows.Forms.Button();
+      this.button9 = new System.Windows.Forms.Button();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(12, 46);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "Красный";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click_1);
+      // 
+      // button2
+      // 
+      this.button2.Location = new System.Drawing.Point(12, 75);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(75, 23);
+      this.button2.TabIndex = 1;
+      this.button2.Text = "Зеленый";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // button3
+      // 
+      this.button3.Location = new System.Drawing.Point(12, 104);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(75, 23);
+      this.button3.TabIndex = 2;
+      this.button3.Text = "Синий";
+      this.button3.UseVisualStyleBackColor = true;
+      this.button3.Click += new System.EventHandler(this.button3_Click);
+      // 
+      // button4
+      // 
+      this.button4.Location = new System.Drawing.Point(12, 133);
+      this.button4.Name = "button4";
+      this.button4.Size = new System.Drawing.Size(75, 23);
+      this.button4.TabIndex = 3;
+      this.button4.Text = "Желтый";
+      this.button4.UseVisualStyleBackColor = true;
+      this.button4.Click += new System.EventHandler(this.button4_Click);
+      // 
+      // button5
+      // 
+      this.button5.Location = new System.Drawing.Point(127, 46);
+      this.button5.Name = "button5";
+      this.button5.Size = new System.Drawing.Size(85, 23);
+      this.button5.TabIndex = 4;
+      this.button5.Text = "Привет";
+      this.button5.UseVisualStyleBackColor = true;
+      this.button5.Click += new System.EventHandler(this.button5_Click);
+      // 
+      // button6
+      // 
+      this.button6.Location = new System.Drawing.Point(127, 75);
+      this.button6.Name = "button6";
+      this.button6.Size = new System.Drawing.Size(85, 23);
+      this.button6.TabIndex = 5;
+      this.button6.Text = "До свидания";
+      this.button6.UseVisualStyleBackColor = true;
+      this.button6.Click += new System.EventHandler(this.button6_Click);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(124, 114);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(35, 13);
+      this.label1.TabIndex = 6;
+      this.label1.Text = "label1";
+      // 
+      // button7
+      // 
+      this.button7.Location = new System.Drawing.Point(257, 46);
+      this.button7.Name = "button7";
+      this.button7.Size = new System.Drawing.Size(100, 23);
+      this.button7.TabIndex = 7;
+      this.button7.Text = "Скрыть";
+      this.button7.UseVisualStyleBackColor = true;
+      this.button7.Click += new System.EventHandler(this.button7_Click);
+      // 
+      // button8
+      // 
+      this.button8.Location = new System.Drawing.Point(257, 75);
+      this.button8.Name = "button8";
+      this.button8.Size = new System.Drawing.Size(100, 23);
+      this.button8.TabIndex = 8;
+      this.button8.Text = "Показать";
+      this.button8.UseVisualStyleBackColor = true;
+      this.button8.Click += new System.EventHandler(this.button8_Click_1);
+      // 
+      // button9
+      // 
+      this.button9.Location = new System.Drawing.Point(257, 104);
+      this.button9.Name = "button9";
+      this.button9.Size = new System.Drawing.Size(100, 23);
+      this.button9.TabIndex = 9;
+      this.button9.Text = "Очистить";
+      this.button9.UseVisualStyleBackColor = true;
+      this.button9.Click += new System.EventHandler(this.button9_Click);
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(257, 133);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(100, 20);
+      this.textBox1.TabIndex = 10;
+      this.textBox1.VisibleChanged += new System.EventHandler(this.button8_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+      this.ClientSize = new System.Drawing.Size(496, 450);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.button9);
+      this.Controls.Add(this.button8);
+      this.Controls.Add(this.button7);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.button6);
+      this.Controls.Add(this.button5);
+      this.Controls.Add(this.button4);
+      this.Controls.Add(this.button3);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.button1);
+      this.Name = "Form1";
+      this.Text = "Практичкская работа 1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.Button button4;
+    private System.Windows.Forms.Button button5;
+    private System.Windows.Forms.Button button6;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Button button7;
+    private System.Windows.Forms.Button button8;
+    private System.Windows.Forms.Button button9;
+    private System.Windows.Forms.TextBox textBox1;
+  }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      MessageBox.Show("Привет!");
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      label1.Text = "Начало работы";
+      BackColor = Color.AntiqueWhite;
+    }
+
+    private void button1_Click_1(object sender, EventArgs e)
+    {
+      BackColor = Color.Red;
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      BackColor = Color.Green;
+    }
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+      BackColor = Color.Blue;
+    }
+
+    private void button4_Click(object sender, EventArgs e)
+    {
+      BackColor = Color.Yellow;
+    }
+
+    private void button5_Click(object sender, EventArgs e)
+    {
+      label1.Text = "Привет";
+    }
+
+    private void button6_Click(object sender, EventArgs e)
+    {
+      label1.Text = "До свидания";
+    }
+
+    private void button8_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void button7_Click(object sender, EventArgs e)
+    {
+      textBox1.Visible = false;
+    }
+
+    private void button8_Click_1(object sender, EventArgs e)
+    {
+      textBox1.Visible = true;
+    }
+
+    private void button9_Click(object sender, EventArgs e)
+    {
+      textBox1.Clear();
+    }
+  }
+}
